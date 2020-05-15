9be392ae158f40f78d316834b2021446725f98b2
root <nauyan@hotmail.com>
2020-05-15 19:32:27 +0500

    Final Day

---
+++ test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+model="frcnn"
+
+ 
+if [ "${model,,}" == "retina" ] 
+then
+    echo "RETINA"
+	conda activate Train
+	#CUDA_VISIBLE_DEVICES=0 python code/keras-retinanet/keras_retinanet/bin/train.py csv train.txt classes.txt --val-annotations vald.txt
+	conda deactivate
+	
+elif [ "${model,,}" == "frcnn" ]
+then
+    echo "FRCNN"
+	conda activate FRCNN
+	CUDA_VISIBLE_DEVICES=0 python code/keras-frcnn/test_frcnn.py -p dataset/SIMS/images/
+	conda deactivate
+	
+elif [ "${model,,}" == "yolo" ]
+then
+    echo "YOLO"
+	conda activate ACV2YOLO
+	#CUDA_VISIBLE_DEVICES=0 python code/keras-yolo3/train.py
+	conda deactivate
+else
+    echo "Select Valid Model"
+fi
